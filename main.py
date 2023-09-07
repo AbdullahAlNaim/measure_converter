ee2ab5c2478550d3464feb93b6d3ee92a3f6beea
Abdullah Naim <aanaim96@gmail.com>
2023-09-06 21:11:27 -0400

    Add files via upload
    rounded all answers with 2 decimal places and added the foot to meter converting option. Would like to consider a GUI to practice building something with that.

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
 def fToC(t):
-    c = (t -32)/ 1.8
+    c = round((t -32)/ 1.8, 2)
     print(c, 'Celsius')
 
 def cToF(t):
-    f = (t * 1.8) + 32
+    f = round((t * 1.8) + 32)
     print(f, 'Fahrenheits')
 
 def inchToCent(m):
-    inches = m * 2.54
+    inches = round(m * 2.54,2)
     print(inches, 'Centimeters')    
 
 def centToInch(m):
-    cm = m / 2.54
-    print(cm, 'Inches')  
+    cm = round(m / 2.54, 2)
+    print(cm, 'Inches') 
 
-ans = input('What would you like to convert: \nA: Fahrenheit to Celsius \nB: Celsius to Fahrenheit \nC: Inches to Centemeters\nD: Centemeters to Inches\n')
+def footToMeter(m):
+    meter = round(m * .3048, 2)
+    print(meter)    
+
+ans = input('What would you like to convert: \nA: Fahrenheit to Celsius \nB: Celsius to Fahrenheit \nC: Inches to Centemeters\nD: Centemeters to Inches\nE: Foot to Meters\n')
 
 if ans == 'a' or ans == 'A':  
-    t = int(input('how many fahrenheits?\n'))
+    t = int(input('how many fahrenheits would you like to convert?\n'))
     fToC(t) 
 elif ans == 'b' or ans == 'B':  
-    t = int(input('how many celsius?\n'))
+    t = int(input('how many celsius would you like to convert?\n'))
     cToF(t)   
 elif ans == 'c' or ans == 'C':
-    m = int(input('how many inches?\n'))
+    m = int(input('how many inches would you like to convert?\n'))
     inchToCent(m)
 elif ans == 'd' or ans == 'D':
-    m = int(input('how many centimeters?\n'))
+    m = int(input('how many centimeters would you like to convert?\n'))
     centToInch(m)
+elif ans == 'E' or ans == 'e':
+    m = int(input('how many feet would you like to convert?\n'))    
+    footToMeter(m)
